52d7fc00c52710a6a5e4bdfef077316479f4b3a4
Pradyumna Krishna <git@onpy.in>
2024-03-18 14:54:53 +0530

    Adds Global site settings to App
    To include emblem, global site settings has been added.
    
    Signed-off-by: Pradyumna Krishna <git@onpy.in>

---
+++ settings/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from wagtail.admin.panels import FieldPanel
+from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
+
+
+@register_setting
+class SiteSettings(BaseSiteSetting):
+    logo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [
+        FieldPanel("logo"),
+    ]
